7520296025a4097fca67a0a011d81489970d538d
Lubna Haitham <41009407+lubnahaitham@users.noreply.github.com>
2022-01-12 14:36:26 +0200

    Update admin.py

--- rest/admin.py
+++ rest/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-from rest.models import Users, Products, Seller
 # Register your models here.
 
-admin.site.register(Users)
-admin.site.register(Products)
-admin.site.register(Seller)
 
 
